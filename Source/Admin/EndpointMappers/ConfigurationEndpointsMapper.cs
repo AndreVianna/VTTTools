a341e94e369cdacd7ec39a627c2f8b129f46192e
Andre Vianna <andre.vianna.rj@hotmail.com>
2025-11-04 17:38:34 -0500

    Walls are done, System Config also done

---
+++ Source/Admin/EndpointMappers/ConfigurationEndpointsMapper.cs
@@ -0,0 +1,22 @@
+namespace VttTools.Admin.EndpointMappers;
+
+public static class ConfigurationEndpointsMapper {
+    public static IEndpointRouteBuilder MapConfigurationEndpoints(this IEndpointRouteBuilder app) {
+        var group = app.MapGroup("/api/admin/configuration")
+            .RequireAuthorization()
+            .RequireRateLimiting("admin");
+
+        group.MapGet("/{serviceName}", ConfigurationHandlers.GetConfigurationHandler)
+            .WithName("GetConfiguration")
+            .RequireAuthorization(policy => policy.RequireRole("Administrator"))
+            .WithOpenApi();
+
+        group.MapPost("/reveal", ConfigurationHandlers.RevealConfigValueHandler)
+            .WithName("RevealConfigValue")
+            .RequireAuthorization(policy => policy.RequireRole("Administrator"))
+            .RequireRateLimiting("reveal")
+            .WithOpenApi();
+
+        return app;
+    }
+}
